900c6fcc76cfdef88bccccfa5418f805414ea808
DESKTOP-M3B5PNR\임현진 <hyunjin6510@gmail.com>
2024-12-02 15:13:23 +0900

    new items

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',  // GitHub Pages를 위한 base URL 설정
+  base: '/youtaxpro/',  // GitHub Pages를 위한 base URL 설정
+  플러그인 : [], 
+  resolve : { 
+    별칭 : [ 
+
+    ]
+  },
 })
