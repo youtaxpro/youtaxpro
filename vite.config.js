aa8037295820e4e08f4f1b783c2e5cffb84406d4
DESKTOP-M3B5PNR\임현진 <hyunjin6510@gmail.com>
2024-12-06 14:03:06 +0900

    'update2'

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/youtaxpro/',  // 이미 설정되어 있네요. 이걸 그대로 사용하시면 됩니다
+  plugins: [
+    vue(),
+    vueI18n({
+      include: path.resolve(__dirname, "./path/to/locales/**"),
+    }),
+  ],
+  base: "/youtaxpro/", // 이미 설정되어 있네요. 이걸 그대로 사용하시면 됩니다
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
