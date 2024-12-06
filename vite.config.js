0400e76e239e1cc49b85e6e60a2a58b3646c739b
DESKTOP-M3B5PNR\임현진 <hyunjin6510@gmail.com>
2024-12-06 16:20:17 +0900

    module install

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import vueI18n from "@intlify/vite-plugin-vue-i18n";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";  // 이 부분 변경
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueI18n({
+    VueI18nPlugin({  // 이 부분 변경
       include: path.resolve(__dirname, "./path/to/locales/**"),
     }),
   ],
