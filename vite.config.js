a3db74603582e0d82cf1baf436ef8aa783dcc6ef
DESKTOP-M3B5PNR\임현진 <hyunjin6510@gmail.com>
2024-12-08 12:23:56 +0900

    20241208 update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";  // 이 부분 변경
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
 
 export default defineConfig({
   plugins: [
     vue(),
-    VueI18nPlugin({  // 이 부분 변경
+    VueI18nPlugin({
       include: path.resolve(__dirname, "./path/to/locales/**"),
     }),
   ],
-  base: "/youtaxpro/",
+  base: "/", // 여기를 수정! 커스텀 도메인을 사용할 때는 그냥 "/"로 설정
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
